c71c58e51ce34e977c406eaf64da074bb72d967b
Navya114 <124689423+Navya114@users.noreply.github.com>
2023-11-13 16:46:40 +0530

    Add files via upload

---
+++ ec2/var.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+  default = "ami-0287a05f0ef0e9d9a"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "demo1"
+}
+
+variable "tags" {
+  default = "instance 1"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
